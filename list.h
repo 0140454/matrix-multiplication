a9b2db023790750993ab6e40103b0c220178d869
kaizsv <kai.zsv@gmail.com>
2016-11-19 01:50:40 +0800

    code refacting

--- list.h
+++ list.h
@@ -1,7 +1,12 @@
 #ifndef __ALGO_LIST_H
 #define __ALGO_LIST_H
 
-#define FUNC_REGISTER(name) \
+#define FUNC_BEGIN(name) \
+	void name ## _multiply(int *src1, int *src2, int *dst, int src1_w, int src1_h, int src2_w, int src2_h)
+
+#if defined(benchmark)
+
+#define FUNC_END(name) \
 		algo_t name = { .type = #name, .join = name ## _multiply, .pNext = NULL }; \
 		__attribute__((constructor)) void append_##name() { \
 			if (list == NULL) { list = &name; } \
@@ -12,17 +17,9 @@
 			} \
 		}
 
-#define FUNC_BEGIN(name) \
-	void name ## _multiply(int *src1, int *src2, int *dst, int src1_w, int src1_h, int src2_w, int src2_h) {
-
-#if defined(benchmark)
-
-#define FUNC_END(name) } \
-		FUNC_REGISTER(name)
-
 #else
 
-#define FUNC_END(name) }
+#define FUNC_END(name)
 
 #endif
 
